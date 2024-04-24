c7be4dcd98ec780e7b7d6f2b8867340d27e5dcb9
QINJIN <953373752@qq.com>
2024-04-24 16:43:29 +0800

    deploy

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "spark-utils-doc",
+  base: "/spark-utils-doc/",
   title: "spark-utils",
   description: "spark-utils",
   themeConfig: {
